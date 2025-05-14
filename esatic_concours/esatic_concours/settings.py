83b0fb77ab1f3eb43b8f4d890ef8bdbf098ef12c
AssemienDev <139700557+AssemienDev@users.noreply.github.com>
2025-05-14 09:49:12 +0000

    version 1

--- esatic_concours/esatic_concours/settings.py
+++ esatic_concours/esatic_concours/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-vzu58p+#j$r3@)h3s%gg(((p6_l2zxy*76u_bydw=njxm9=3tb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -79,8 +82,11 @@ WSGI_APPLICATION = 'esatic_concours.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
     }
 }
 
